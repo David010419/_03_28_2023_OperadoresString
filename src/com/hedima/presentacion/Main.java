43199dac5299a9d0d4778b071c644dd7c2cb17fc
David010419 <dcastillo@global-retail.com>
2023-03-28 21:15:52 +0200

    Proyecto a las 21:00

---
+++ src/com/hedima/presentacion/Main.java
@@ -0,0 +1,57 @@
+package com.hedima.presentacion;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+        //Asignacion de variables
+        int a = 10;
+//         a=a+5;
+        a+=5;
+        System.out.println(a);
+
+        int b = 10;
+//         b=b+5;
+        b-=5;
+        System.out.println(b);
+        //Resto
+        int c = 10;
+        System.out.println("resto es ->" + (c%3));
+        //Incremento
+        int d = 10;
+//        d+=1;
+        d++;
+        System.out.println(d);
+
+        //Pre-incremento y post-incremento
+//        int e = 10;
+//        int f = 5;
+//        e+=f++;
+//        System.out.println(e);
+//        System.out.println(f);
+        //post incremento le asigna valor a la e y luego incrementa la f
+
+        int e = 10;
+        int f = 5;
+        e+=++f;
+        System.out.println(e);
+        System.out.println(f);
+        //pre incremento le asigna valor a la e, la incrementa y incrementa la f
+
+//        int g = 10;
+//        int h = 5;
+//        g+=h--;
+//        System.out.println(g);
+//        System.out.println(h);
+
+        int g = 10;
+        int h = 5;
+        g+=--h;
+        System.out.println(g);
+        System.out.println(h);
+
+
+    }
+}
+
---
+++ src/com/hedima/presentacion/ProbarClaseString.java
@@ -0,0 +1,15 @@
+package com.hedima.presentacion;
+
+public class ProbarClaseString {
+    public static void main(String[] args) {
+
+        //char letra = 'a';
+        //String comillas dobles "" y char comillas simples ''
+        String nombre = "Juan";
+        String apellido = "Perez";
+        System.out.println(nombre+ " "+apellido);
+        System.out.println(nombre.concat( " "+apellido));
+
+
+    }
+}
